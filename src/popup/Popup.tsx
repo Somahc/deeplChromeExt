bf4652a848e725bdf5857cb59c8d70897deaf8f3
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-06-25 23:01:45 +0900

    Popupを編集

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -5,9 +5,16 @@ const Popup = () => {
 
   return (
     <>
-      <div className="flex justify-center mt-2 text-base">Popup Counter</div>
-      <Counter />
-      kjn
+      <div className="flex justify-center mt-2 text-base">
+        <p>どの言語に翻訳しますか？</p>
+        <select name="target_lang">
+          <option value="EN">英語</option>
+          <option value="KO">韓国語</option>
+          <option value="ZH">中国語</option>
+          <option value="JA">日本語</option>
+        </select>
+      </div>
+      {/* <Counter /> */}
     </>
   );
 };
