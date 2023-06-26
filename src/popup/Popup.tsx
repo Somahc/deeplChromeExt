c5f8c517e63fc7220a6c83df68899050087b0ec6
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-06-26 17:16:41 +0900

    言語選択情報が保存されるようにした

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,15 +1,39 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
+import { getBucket } from '@extend-chrome/storage';
 import { Container, Select } from '@mantine/core';
 
-const Popup = () => {
+interface MyBucket {
+  targetLang: string;
+}
+
+const bucket = getBucket<MyBucket>('my_bucket', 'sync');
+
+const Popup = (): ReactElement => {
   document.body.style.width = '20rem';
   document.body.style.height = '20rem';
+
+  const [lang, setLang] = useState('EN');
+
+  useEffect(() => {
+    (async () => {
+      const value = await bucket.get();
+      if (value.targetLang) {
+        setLang(value.targetLang);
+      }
+    })();
+  }, []);
+
+  const saveLang = (lang: string) => {
+    bucket.set({ targetLang: lang });
+    setLang(lang);
+  };
 
   return (
     <Container p="xl">
       <Select
         label="どの言語に翻訳しますか？"
-        defaultValue="EN"
+        value={lang}
+        onChange={(value: string) => saveLang(value)}
         data={[
           { value: 'EN', label: '英語' },
           { value: 'KR', label: '韓国語' },
