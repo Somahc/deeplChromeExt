82802e107be66df0e33d2905a96765d194127ffc
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-06-29 19:36:00 +0900

    今テキストメニューに追加、選択したテキストの取得

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,19 +1,19 @@
-import browser from 'webextension-polyfill';
-import store, { initializeWrappedStore } from '../app/store';
-
-initializeWrappedStore();
-
-store.subscribe(() => {
-  // access store state
-  // const state = store.getState();
-  // console.log('state', state);
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'translation',
+    title: '選択したテキストを翻訳',
+    contexts: ['selection'],
+  });
 });
 
-// show welcome page on new install
-browser.runtime.onInstalled.addListener(async (details) => {
-  if (details.reason === 'install') {
-    //show the welcome page
-    const url = browser.runtime.getURL('welcome/welcome.html');
-    await browser.tabs.create({ url });
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+  if (tab !== undefined) {
+    switch (info.menuItemId) {
+      case 'translation':
+        console.log(info.selectionText);
+        break;
+    }
   }
 });
+
+export {};
